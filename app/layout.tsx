3bb4255b17c8ccc91a00e02b51a9a3ab3e5f90ff
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2025-07-15 17:39:54 +0530

    Final Commitssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aryan's Portfolio",
   description: "A modern & minimalist portfolio",
+  icons:{
+    icon:"/favicon.ico",
+  }
 };
 
 export default function RootLayout({
